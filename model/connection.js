64a0ca65acb65d05611f6ebe273ad1674091a1ea
Vitor Lima <Vitorblsantos@gmail.com>
2020-04-07 14:28:09 -0300

    change arch

--- model/connection.js
+++ model/connection.js
@@ -1,5 +1,7 @@
-const sqlite3 = require('sqlite3');
+const sqlite3 = require("sqlite3");
 
-const connection = new sqlite3.Database('db.sql');
+const connection = new sqlite3.Database("../db/db.sql", (err) =>
+  console.error("db con error", err)
+);
 
 module.exports = connection;
