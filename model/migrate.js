7ce2e874f26b0564ebd55e38a0536beaa54f450b
Vitor Lima <Vitorblsantos@gmail.com>
2020-04-08 10:38:30 -0300

    hotfix - migrate

--- model/migrate.js
+++ model/migrate.js
@@ -1,16 +1,16 @@
-const Connection = require('./connection')
+const Connection = require("./connection");
 
-const db = new Connection()
+const db = new Connection();
 
 const data = [
-  ['GRU', 'BRC', 10],
-  ['GRU', 'SCL', 18],
-  ['GRU', 'ORL', 56],
-  ['GRU', 'CDG', 75],
-  ['SCL', 'ORL', 20],
-  ['BRC', 'SCL', 5],
-  ['ORL', 'CDG', 5]
-]
+  ["GRU", "BRC", 10],
+  ["GRU", "SCL", 18],
+  ["GRU", "ORL", 56],
+  ["GRU", "CDG", 75],
+  ["SCL", "ORL", 20],
+  ["BRC", "SCL", 5],
+  ["ORL", "CDG", 5],
+];
 
 try {
   db.run(
@@ -23,22 +23,22 @@ try {
     )
   `,
     (err) => {
-      if (err) return
-      db.all('SELECT count(*) as count FROM routes', (err, rows) => {
-        if (!err) return
-        const { count } = rows[0]
-        if (count === 0) {
-          data.map((single) => {
-            db.all(
-              'INSERT INTO routes(origin,dest,price,status) VALUES(?,?,?,?)',
-              [single[0], single[1], single[2], 1],
-              (err) => err && console.log(err)
-            )
-          })
-        }
-      })
+      if (err) return;
+      db.all("SELECT count(*) as count FROM routes", (err, rows) => {
+        if (err) return;
+        const { count } = rows[0];
+        if (count > 0) return;
+        data.map(async (single) => {
+          return await db.all(
+            "INSERT INTO routes(origin,dest,price,status) VALUES(?,?,?,?)",
+            [single[0], single[1], single[2], true],
+            (err) => err 
+          );
+        });
+        console.info("Migration complete")
+      });
     }
-  )
+  );
 } catch {
-  console.error('Migration failed')
+  console.error("Migration failed");
 }
