7befd2e6b1ab46ee8ebdca0ace2cc359beec0ccd
Vitor Lima <Vitorblsantos@gmail.com>
2020-04-08 06:25:34 -0300

    remove comment

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,26 @@
-const { route, quote } = require('../controller')
-const router = require('express').Router()
+const { route, quote } = require("../controller");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-  res.send('<h1>Server Working</h1>')
-})
+router.get("/", (req, res) => {
+  res.send("<h1>Server Working</h1>");
+});
 
-router.get('/quote/:from/:to', async (req, res) => {
+router.get("/quote/:from/:to", async (req, res) => {
   try {
-    const { from, to } = req.params
-    // await quote(from, to);
-    res.send(await quote(from, to))
+    const { from, to } = req.params;
+    res.send(await quote(from, to));
   } catch {
-    res.send('Controller error - Quote')
+    res.send("Controller error - Quote");
   }
-})
+});
 
-router.post('/route', async (req, res) => {
+router.post("/route", async (req, res) => {
   try {
-    const { from, to, price } = req.body
-    res.send(await route(from, to, price))
+    const { from, to, price } = req.body;
+    res.send(await route(from, to, price));
   } catch {
-    res.send('Controller error - Route')
+    res.send("Controller error - Route");
   }
-})
+});
 
-module.exports = router
+module.exports = router;
