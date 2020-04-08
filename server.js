b85447f6b35b5385214cfe8183360dcab3da9da3
Vitor Lima <Vitorblsantos@gmail.com>
2020-04-08 06:36:48 -0300

    fix server var error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser')
 const express = require('express')()
 const router = require('./routes/')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+express.use(bodyParser.urlencoded({ extended: false }));
 express.use(bodyParser.json())
 express.use('/', router)
 express.listen(1998)
