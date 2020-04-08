5e012171e0963d25ccfe8d841b0363e43e4051ea
Vitor Lima <Vitorblsantos@gmail.com>
2020-04-08 10:16:39 -0300

    standard

--- test/quote/quote.js
+++ test/quote/quote.js
@@ -1,25 +1,25 @@
-const App = require("../../server");
-const chai = require("chai");
-const expect = chai.expect;
-const chaiHttp = require("chai-http");
+const App = require('../../server')
+const chai = require('chai')
+const expect = chai.expect
+const chaiHttp = require('chai-http')
 
-const { quoteVerified } = require("./quote.json");
+const { quoteVerified } = require('./quote.json')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
-describe("Quote - Endpoint", () => {
-  it("Check verified route - GET /quote", (done) => {
-    const { from, to } = quoteVerified;
+describe('Quote - Endpoint', () => {
+  it('Check verified route - GET /quote', (done) => {
+    const { from, to } = quoteVerified
     chai
       .request(App)
       .get(`/quote/${from}/${to}`)
       .end((err, res) => {
-        if (err) return;
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.a("object");
-        expect(res.body.route).to.be.an("string");
-        expect(res.body.price).to.be.an("number");
-        done();
-      });
-  });
-});
+        if (err) return
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.a('object')
+        expect(res.body.route).to.be.an('string')
+        expect(res.body.price).to.be.an('number')
+        done()
+      })
+  })
+})
