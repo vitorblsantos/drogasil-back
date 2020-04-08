b944a87d7602e9f9476e04f3b35ecddb6ee1aeb5
Vitor Lima <Vitorblsantos@gmail.com>
2020-04-08 06:55:26 -0300

    setup mocha

---
+++ test/integration/route.js
@@ -0,0 +1,13 @@
+describe("Route - Endpoint", () => {
+  describe("POST /route", () => {
+    it("200", (done) => {
+      done();
+    });
+    it("303", (done) => {
+      done();
+    });
+    it("500", (done) => {
+      done();
+    });
+  });
+});
