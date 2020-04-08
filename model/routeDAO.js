7a42d27d4e086646f662c7ef12373e50670ebd5c
Vitor Lima <Vitorblsantos@gmail.com>
2020-04-08 10:20:17 -0300

    standard --fix

--- model/routeDAO.js
+++ model/routeDAO.js
@@ -1,38 +1,38 @@
-const Connection = require("./connection");
+const Connection = require('./connection')
 
-const db = new Connection();
+const db = new Connection()
 
 const saveRoute = async (origin, destination, price) => {
   if (origin && destination && price) {
     return new Promise((resolve, reject) => {
       try {
         db.all(
-          "SELECT count(*) as count FROM routes where origin = ? and dest = ?",
+          'SELECT count(*) as count FROM routes where origin = ? and dest = ?',
           [origin, destination],
           (err, rows) => {
             if (!err) {
-              const { count } = rows[0];
-              if (count > 0) return resolve("value duplicated");
+              const { count } = rows[0]
+              if (count > 0) return resolve('value duplicated')
               db.run(
-                "INSERT INTO routes(origin,dest,price,status)values(?,?,?,?)",
+                'INSERT INTO routes(origin,dest,price,status)values(?,?,?,?)',
                 [origin, destination, price, true],
-                (err) => err || resolve("route saved")
-              );
+                (err) => err || resolve('route saved')
+              )
             }
           }
-        );
+        )
       } catch (e) {
-        reject(e);
+        reject(e)
       }
-    });
+    })
   } else {
-    return "some params could not be read";
+    return 'some params could not be read'
   }
-};
+}
 
-const getRoute = () => {};
+const getRoute = () => {}
 
 module.exports = {
   saveRoute,
-  getRoute,
-};
+  getRoute
+}
